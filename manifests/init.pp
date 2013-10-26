3aa136871ff6bf3de96b9d89bed0e2f4dcb83950
Reid Vandewiele <reid@puppetlabs.com>
2013-10-26 23:09:08 +0000

    Remove rote management of conf.d
    It was originally put in this way to allow purging of the directory;
    removal of once-roles no longer applied. However, the management pattern
    was preventing upgrades or moving forward. Removing the ability for now to
    purge the directory in favor of enforcing desired state (at the expense of
    anti-state).

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,32 +19,13 @@ class pe_httpd (
     require => Service['pe-memcached'],
   }
 
-  # PATCH: manage missing shebang in init script for Debian packaging
-  if $::osfamily == 'Debian' {
-    $memcached = '/etc/init.d/pe-memcached'
-    exec { 'insert-missing-shebang-for-pe-memcached':
-      path     => '/bin:/usr/bin',
-      provider => shell,
-      command  => "f=$(echo '#!/bin/bash' | cat - ${memcached}); echo \"\$f\" > ${memcached}",
-      unless   => "grep -q '#!/bin/' $memcached",
-      before   => Service['pe-httpd'],
-      require  => Package['pe-httpd'],
-    }
-  }
-
-  # Manage the conf.d directory. This should probably be split out from the
-  # (non-existent on RedHat) sites-enabled directory. Then we wouldn't have
-  # to ship the default conf files in the module.
   file { '/etc/puppetlabs/httpd/conf.d':
     ensure  => directory,
-    purge   => true,
-    recurse => true,
-    force   => true,
     owner   => '0',
     group   => '0',
     mode    => '0644',
-    source  => "puppet:///modules/pe_httpd/conf.d/${::osfamily}",
     notify  => Service['pe-httpd'],
+    require => Package['pe-httpd'],
   }
 
 }
